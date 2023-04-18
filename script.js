1a0190d136e2cab594cfe601b59c1e8c409b7d87
d0n1x <131188077+d0n1x@users.noreply.github.com>
2023-04-18 16:26:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function calcular(){
+    var numero1=document.getElementsByClassName('input1').value
+    var numero2=document.getElementsByClassName('input2').value
+    var soma=numero1+numero2;
+}
